29975a85eccf8e78f80e0a45481994afc1bd3bcd
admin.pdeja <pdeja@9bits.pl>
2024-04-07 15:20:15 +0200

    Create set-itinerary component

---
+++ src/app/3-set-itinerary/set-itinerary/set-itinerary.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'iy-set-itinerary',
+  standalone: true,
+  imports: [],
+  templateUrl: './set-itinerary.component.html',
+  styleUrl: './set-itinerary.component.scss'
+})
+export class SetItineraryComponent {
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SectionHeadingComponent } from './_shared/components/section-heading/section-heading.component';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SectionHeadingComponent
+  ]
+})
+export class AppModuleModule { }
